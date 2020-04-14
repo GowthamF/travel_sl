570607f63b7e60995b0dc47acdea5806f097b635
GowthamF <40605124+GowthamF@users.noreply.github.com>
2020-04-14 19:52:12 +0530

    changes

--- lib/widgets/translator.dart
+++ lib/widgets/translator.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:travel_sl/widgets/translator/camera.dart';
 
 class Translator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Translator'),
-        ),
-      ),
-    );
+    return TakePictureScreen();
   }
 }
--- lib/widgets/translator/camera.dart
+++ lib/widgets/translator/camera.dart
@@ -2,39 +2,18 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:camera/camera.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
-
-Future<void> main() async {
-  // Ensure that plugin services are initialized so that `availableCameras()`
-  // can be called before `runApp()`
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Obtain a list of the available cameras on the device.
-  final cameras = await availableCameras();
-
-  // Get a specific camera from the list of available cameras.
-  final firstCamera = cameras.first;
-
-  runApp(
-    MaterialApp(
-      theme: ThemeData.dark(),
-      home: TakePictureScreen(
-        // Pass the appropriate camera to the TakePictureScreen widget.
-        camera: firstCamera,
-      ),
-    ),
-  );
-}
+import 'package:travel_sl/singletons/translator_singleton.dart';
+import 'package:travel_sl/widgets/scanner_utils.dart';
 
 // A screen that allows users to take a picture using a given camera.
 class TakePictureScreen extends StatefulWidget {
-  final CameraDescription camera;
-
   const TakePictureScreen({
     Key key,
-    @required this.camera,
   }) : super(key: key);
 
   @override
@@ -42,99 +21,238 @@ class TakePictureScreen extends StatefulWidget {
 }
 
 class TakePictureScreenState extends State<TakePictureScreen> {
-  CameraController _controller;
+  dynamic _scanResults;
+  CameraController _camera;
+  Detector _currentDetector = Detector.text;
+  bool _isDetecting = false;
+  CameraLensDirection _direction = CameraLensDirection.back;
   Future<void> _initializeControllerFuture;
+
+  final BarcodeDetector _barcodeDetector =
+      FirebaseVision.instance.barcodeDetector();
+  final FaceDetector _faceDetector = FirebaseVision.instance.faceDetector();
+  final ImageLabeler _imageLabeler = FirebaseVision.instance.imageLabeler();
+  final ImageLabeler _cloudImageLabeler =
+      FirebaseVision.instance.cloudImageLabeler();
+  final TextRecognizer _recognizer = FirebaseVision.instance.textRecognizer();
+  final TextRecognizer _cloudRecognizer =
+      FirebaseVision.instance.cloudTextRecognizer();
 
   @override
   void initState() {
     super.initState();
-    // To display the current output from the Camera,
-    // create a CameraController.
-    _controller = CameraController(
-      // Get a specific camera from the list of available cameras.
-      widget.camera,
-      // Define the resolution to use.
-      ResolutionPreset.high,
+    _initializeCamera();
+  }
+
+  void _initializeCamera() async {
+    final CameraDescription description =
+        await ScannerUtils.getCamera(_direction);
+
+    _camera = CameraController(
+      description,
+      defaultTargetPlatform == TargetPlatform.iOS
+          ? ResolutionPreset.low
+          : ResolutionPreset.medium,
+    );
+    await _camera.initialize();
+
+    _camera.startImageStream((CameraImage image) {
+      if (_isDetecting) return;
+
+      _isDetecting = true;
+
+      ScannerUtils.detect(
+        image: image,
+        detectInImage: _getDetectionMethod(),
+        imageRotation: description.sensorOrientation,
+      ).then(
+        (dynamic results) {
+          if (_currentDetector == null) return;
+          setState(() {
+            _scanResults = results;
+          });
+        },
+      ).whenComplete(() => _isDetecting = false);
+    });
+  }
+
+  Future<dynamic> Function(FirebaseVisionImage image) _getDetectionMethod() {
+    switch (_currentDetector) {
+      case Detector.text:
+        return _recognizer.processImage;
+      case Detector.cloudText:
+        return _cloudRecognizer.processImage;
+      case Detector.barcode:
+        return _barcodeDetector.detectInImage;
+      case Detector.label:
+        return _imageLabeler.processImage;
+      case Detector.cloudLabel:
+        return _cloudImageLabeler.processImage;
+      case Detector.face:
+        return _faceDetector.processImage;
+    }
+
+    return null;
+  }
+
+  Widget _buildResults() {
+    const Text noResultsText = Text('No results!');
+
+    if (_scanResults == null ||
+        _camera == null ||
+        !_camera.value.isInitialized) {
+      return noResultsText;
+    }
+
+    CustomPainter painter;
+
+    final Size imageSize = Size(
+      _camera.value.previewSize.height,
+      _camera.value.previewSize.width,
     );
 
-    // Next, initialize the controller. This returns a Future.
-    _initializeControllerFuture = _controller.initialize();
-    setState(() {});
+    assert(_currentDetector == Detector.text ||
+        _currentDetector == Detector.cloudText);
+    if (_scanResults is! VisionText) return noResultsText;
+
+    return CustomPaint(
+      painter: painter,
+    );
   }
 
-  @override
-  void dispose() {
-    // Dispose of the controller when the widget is disposed.
-    _controller.dispose();
-    super.dispose();
+  Widget _buildImage() {
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      child: _camera == null
+          ? const Center(
+              child: Text(
+                'Initializing Camera...',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 30.0,
+                ),
+              ),
+            )
+          : Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                CameraPreview(_camera),
+                _buildResults(),
+              ],
+            ),
+    );
   }
 
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      _controller != null
-          ? _initializeControllerFuture = _controller.initialize()
-          : null; //on pause camera is disposed, so we need to call again "issue is only for android"
+  void _toggleCameraDirection() async {
+    if (_direction == CameraLensDirection.back) {
+      _direction = CameraLensDirection.front;
+    } else {
+      _direction = CameraLensDirection.back;
     }
+
+    await _camera.stopImageStream();
+    await _camera.dispose();
+
+    setState(() {
+      _camera = null;
+    });
+
+    _initializeCamera();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Take a picture')),
-      // Wait until the controller is initialized before displaying the
-      // camera preview. Use a FutureBuilder to display a loading spinner
-      // until the controller has finished initializing.
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, display the preview.
-            return CameraPreview(_controller);
-          } else {
-            // Otherwise, display a loading indicator.
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera_alt),
-        // Provide an onPressed callback.
-        onPressed: () async {
-          // Take the Picture in a try / catch block. If anything goes wrong,
-          // catch the error.
-          try {
-            // Ensure that the camera is initialized.
-            await _initializeControllerFuture;
-
-            // Construct the path where the image should be saved using the
-            // pattern package.
-            final path = join(
-              // Store the picture in the temp directory.
-              // Find the temp directory using the `path_provider` plugin.
-              (await getTemporaryDirectory()).path,
-              '${DateTime.now()}.png',
-            );
-
-            // Attempt to take a picture and log where it's been saved.
-            await _controller.takePicture(path);
-
-            // If the picture was taken, display it on a new screen.
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(imagePath: path),
+      appBar: AppBar(
+        title: const Text('ML Vision Example'),
+        actions: <Widget>[
+          PopupMenuButton<Detector>(
+            onSelected: (Detector result) {
+              _currentDetector = result;
+            },
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<Detector>>[
+              const PopupMenuItem<Detector>(
+                child: Text('Detect Barcode'),
+                value: Detector.barcode,
               ),
-            );
-          } catch (e) {
-            // If an error occurs, log the error to the console.
-            print(e);
-          }
-        },
+              const PopupMenuItem<Detector>(
+                child: Text('Detect Face'),
+                value: Detector.face,
+              ),
+              const PopupMenuItem<Detector>(
+                child: Text('Detect Label'),
+                value: Detector.label,
+              ),
+              const PopupMenuItem<Detector>(
+                child: Text('Detect Cloud Label'),
+                value: Detector.cloudLabel,
+              ),
+              const PopupMenuItem<Detector>(
+                child: Text('Detect Text'),
+                value: Detector.text,
+              ),
+              const PopupMenuItem<Detector>(
+                child: Text('Detect Cloud Text'),
+                value: Detector.cloudText,
+              ),
+            ],
+          ),
+        ],
       ),
+      body: _buildImage(),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            try {
+              // Ensure that the camera is initialized.
+              await _camera.initialize();
+
+              // Construct the path where the image should be saved using the
+              // pattern package.
+              final path = join(
+                // Store the picture in the temp directory.
+                // Find the temp directory using the `path_provider` plugin.
+                (await getTemporaryDirectory()).path,
+                '${DateTime.now()}.png',
+              );
+
+              await Future.delayed(new Duration(seconds: 1));
+
+              // Attempt to take a picture and log where it's been saved.
+              await _camera.takePicture(path);
+
+              // If the picture was taken, display it on a new screen.
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DisplayPictureScreen(imagePath: path),
+                ),
+              );
+            } catch (e) {
+              // If an error occurs, log the error to the console.
+              print(e);
+            }
+          },
+          child: Icon(Icons.camera)),
     );
   }
+
+  @override
+  void dispose() {
+    _camera.dispose().then((_) {
+      _barcodeDetector.close();
+      _faceDetector.close();
+      _imageLabeler.close();
+      _cloudImageLabeler.close();
+      _recognizer.close();
+      _cloudRecognizer.close();
+    });
+
+    _currentDetector = null;
+    super.dispose();
+  }
 }
+
+enum Detector { barcode, face, label, cloudLabel, text, cloudText }
 
 // A widget that displays the picture taken by the user.
 class DisplayPictureScreen extends StatelessWidget {
