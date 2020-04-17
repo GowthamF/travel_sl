8fdc86ec0a20911ab4dbb41e52bedeb4f26823d3
GowthamF <40605124+GowthamF@users.noreply.github.com>
2020-04-17 13:38:49 +0530

    no problems1

--- lib/widgets/trains.dart
+++ lib/widgets/trains.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class Trains extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
